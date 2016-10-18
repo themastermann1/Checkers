f041c88a1739f6d48cbeff40e34210c3509ba616
themastermann1 <sdm29@sussex.ac.uk>
2016-10-18 12:52:41 +0100

    Porting to Lab PC
    Finally managed to get git to play along, can now work on both PC's!

--- src/checkers/AI.java
+++ src/checkers/AI.java
@@ -4,20 +4,30 @@
  */
 package checkers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
  * @author sdm29
  */
 public class AI extends Player{
     Difficulty difficulty;
+    ArrayList<String> names;
     
     AI(String n, Difficulty difficulty){
         super(n); 
+        names.addAll(Arrays.asList("RNJesus","Dicey Dave", "Flipping Fred", "Random Ryan", "Chancy Chad"));
+        names.trimToSize();
         name = rndName();
+        
         this.difficulty = difficulty;
     }
     
     private String rndName(){
-        return("fred");
+        Random ran = new Random();
+        int R = ran.nextInt(names.size());
+        return(names.get(R));
     }
 }
