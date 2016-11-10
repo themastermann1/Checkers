2547cc8df12629a290488da6120b062d26345cec
themastermann1 <sdm29@sussex.ac.uk>
2016-11-10 16:59:21 +0000

    AI Framework
    Additional work done to the AI class extending its framework. The class
    now compiles correctly and generates a blank player with a randomised
    name.
    
    Added test class for AI

--- src/checkers/AI.java
+++ src/checkers/AI.java
@@ -13,21 +13,39 @@ import java.util.Random;
  * @author sdm29
  */
 public class AI extends Player{
-    Difficulty difficulty;
-    ArrayList<String> names;
+    private Difficulty difficulty;
+    private ArrayList<String> names;
     
     AI(String n, Difficulty difficulty){
         super(n); 
-        names.addAll(Arrays.asList("RNJesus","Dicey Dave", "Flipping Fred", "Random Ryan", "Chancy Chad"));
-        names.trimToSize();
-        name = rndName();
+        names = new ArrayList<>();
         
+        initNames();
         this.difficulty = difficulty;
+    }
+    
+    private void initNames(){
+        //change this to stooring the names in a simple .txt file? allows addintion of names to be saved
+        //and names of high scooring players to be added to the list for the AI to pick from
+        //even add thhings like if ben is top scorer add a randomly generated name based on "ben" to the list, 
+        //ie bens dad, chess guru ben, bens ghost etc.
+        this.names.addAll(Arrays.asList("RNJesus","Dicey Dave", "Flipping Fred", "Random Ryan", "Chancy Chad"));
+        this.names.trimToSize();
+        this.name = rndName();
     }
     
     private String rndName(){
         Random ran = new Random();
         int R = ran.nextInt(names.size());
         return(names.get(R));
+    }
+    
+    public void addName(String n){
+        //lets users add names to list that the AI picks from.
+        this.names.add(n);
+    }
+    
+    public String getName(){
+        return this.name;
     }
 }
---
+++ test/checkers/AITest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkers;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sdm29
+ */
+public class AITest {
+    
+    public AITest() {
+    }
+
+    //auto generated shit
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testNames() {
+        Difficulty d = Difficulty.HELL_ON_EARTH;
+        String testName = "replaceMe";
+        AI test = new AI(testName, d);
+        System.out.println(test.name);
+        assertTrue(!(test.getName().equals(testName)));
+    }
+}
